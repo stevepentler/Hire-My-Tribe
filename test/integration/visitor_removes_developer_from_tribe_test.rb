df9b183e1a468fc38c9240a1a4b067cf7020dd57
Steve Pentler <stevepentler@gmail.com>
2016-01-12 19:53:54 -0700

    Incorporates FactoryGirl into new tests

--- test/integration/visitor_removes_developer_from_tribe_test.rb
+++ test/integration/visitor_removes_developer_from_tribe_test.rb
@@ -2,30 +2,29 @@ require 'test_helper'
 
 class VisitorRemovesDeveloperFromTribeTest < ActionDispatch::IntegrationTest
   test "visitor can remove developer from tribe prior to checkout" do
-    developer1 = developers.first
-    developer_last = developers.last
+    dev1, dev2 = create_list(:developer, 2)
 
-    visit developer_path(developer1)
+    visit developer_path(dev1)
 
     click_on "Add to tribe"
-    assert page.has_content?("#{developer1.name} has joined your tribe!")
+    assert page.has_content?("#{dev1.name} has joined your tribe!")
 
-    visit developer_path(developer_last)
+    visit developer_path(dev2)
     
     click_on "Add to tribe"
-    assert page.has_content?("#{developer_last.name} has joined your tribe!")
+    assert page.has_content?("#{dev2.name} has joined your tribe!")
     
     click_on "Current Tribe"
     assert_equal current_path, tribe_path
     
-    assert page.has_content?("#{developer1.name}")
-    assert page.has_content?("#{developer1.name}")
-    within "#developer_#{developer1.name}" do 
+    assert page.has_content?("#{dev1.name}")
+    assert page.has_content?("#{dev1.name}")
+    within "#developer_#{dev1.name}" do 
       click_on "Remove Developer"
     end 
 
-    refute page.has_content?("#{developer1.name}")
-    assert page.has_content?("#{developer_last.name}")
+    refute page.has_content?("#{dev1.name}")
+    assert page.has_content?("#{dev2.name}")
 
     assert_equal tribe_path, current_path
 
