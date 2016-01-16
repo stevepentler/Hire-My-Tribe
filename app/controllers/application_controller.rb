ada5da16f7c75d957c2a29b91a5fee88bdabb696
Hector Huertas <hectorhuertas@gmail.com>
2016-01-16 16:36:38 -0700

    Implement security for admin dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,4 +56,8 @@ class ApplicationController < ActionController::Base
     @current_contractor ||=
           Contractor.find(session[:contractor_id]) if session[:contractor_id]
   end
+
+  def current_admin
+    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
+  end
 end
--- test/integration/admin_has_a_private_dashboard_test.rb
+++ test/integration/admin_has_a_private_dashboard_test.rb
@@ -13,19 +13,63 @@ class AdminHasAPrivateDashboardTest < ActionDispatch::IntegrationTest
     # save_and_open_page
     assert_equal admin_dashboard_path, current_path
     assert page.has_content? "Admin Dashboard"
-      
+
     visit root_path
     visit '/admin/dashboard'
     assert page.has_content? "Admin Dashboard"
   end
-end
 
-# As an Admin
-#       When I visit "/admin/dashboard"
-#       I will see a heading on the page that says "Admin Dashboard"
-#       As a registered user
-#       When I visit "/admin/dashboard"
-#       I get a 404
-#       As an unregistered user
-#       When I visit "/admin/dashboard"
-#       I get a 404
+  test "admin login to their dashboard through developer form" do
+    admin = create(:admin)
+    visit login_path
+
+    within "#developer-login-form" do
+      fill_in "session[email]", with: admin.email
+      fill_in "session[password]", with: admin.password
+      click_on "Developer Login"
+    end
+
+    assert_equal admin_dashboard_path, current_path
+    assert page.has_content? "Admin Dashboard"
+
+    visit root_path
+    visit '/admin/dashboard'
+    assert page.has_content? "Admin Dashboard"
+  end
+
+  test "contractors 404 on admin dashboard" do
+    contractor = create(:contractor)
+    visit login_path
+
+    within "#contractor-login-form" do
+      fill_in "session[email]", with: contractor.email
+      fill_in "session[password]", with: contractor.password
+      click_on "Contractor Login"
+    end
+
+    visit '/admin/dashboard'
+    # save_and_open_page
+    assert page.has_content? "The page you were looking for doesn't exist"
+  end
+
+  test "developers 404 on admin dashboard" do
+    developer = create(:developer)
+    visit login_path
+
+    within "#developer-login-form" do
+      fill_in "session[email]", with: developer.email
+      fill_in "session[password]", with: developer.password
+      click_on "Developer Login"
+    end
+
+    visit '/admin/dashboard'
+
+    assert page.has_content? "The page you were looking for doesn't exist"
+  end
+
+  test "guests 404 on admin dashboard" do
+    visit '/admin/dashboard'
+
+    assert page.has_content? "The page you were looking for doesn't exist"
+  end
+end
