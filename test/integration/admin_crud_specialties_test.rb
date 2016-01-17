0312cba742d27dba8157c3fa727fd1da9bbe90ca
Hector Huertas <hectorhuertas@gmail.com>
2016-01-16 18:55:55 -0700

    Cleaning

--- test/integration/admin_crud_specialties_test.rb
+++ test/integration/admin_crud_specialties_test.rb
@@ -14,7 +14,25 @@ class AdminCrudSpecialtiesTest < ActionDispatch::IntegrationTest
     click_on "Create Specialty"
 
     specialty = Specialty.last
-    save_and_open_page
+
+    assert_equal admin_specialties_path, current_path
+    assert page.has_content? specialty.name
+    assert page.has_css? "#database-image"
+  end
+
+  test "admin creates a specialty without picture" do
+    admin = create(:admin)
+    ApplicationController.any_instance.stubs(:current_admin).returns(admin)
+
+    visit new_admin_specialty_path
+
+    fill_in "Name", with: "database"
+    fill_in "Description", with: ""
+    fill_in "Url name", with: "database"
+    click_on "Create Specialty"
+
+    specialty = Specialty.last
+
     assert_equal admin_specialties_path, current_path
     assert page.has_content? specialty.name
     assert page.has_css? "#database-image"
