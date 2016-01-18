c75f7f5427745aa5df68e1c01b5e05759c24dcae
Aaron Greenspan <afg419@gmail.com>
2016-01-18 16:16:01 -0700

    Changes text size in left developer show card

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -15,7 +15,7 @@ FactoryGirl.define do
 
   factory :project do
     title { FactoryGirl.generate(:project_title) }
-    description " "
+    description "description"
     total 40
     status 0
     created_at Time.now
---
+++ test/integration/project_links_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class ProjectLinksTest < ActionDispatch::IntegrationTest
+  test "developer pages link to developer's projects" do
+    dev = create(:developer)
+    projects = create_list(:project, 3)
+
+    sample_project = projects.sample
+
+    dev.projects += projects
+
+    visit developer_path(dev)
+
+    projects.each do |project|
+      assert page.has_content?(project.title)
+    end
+
+    click_on sample_project.title
+
+    assert_equal project_path(sample_project), current_path
+
+    assert page.has_content?(project.title)
+    assert page.has_content?(project.description)
+    refute page.has_content?(project.status)
+  end
+
+  test "contractor pages link to contractor's projects" do
+
+  end
+
+  test "project pages link to project's developers" do
+
+  end
+
+  test "project pages link to project's contractor" do
+
+  end
+end
