add96c92fa22d4330aa6b119b0cb730ea39be6ec
Aaron Greenspan <afg419@gmail.com>
2016-01-18 20:42:42 -0700

    Computes named language bonuses

--- app/genetic_algorithm/fitness.rb
+++ app/genetic_algorithm/fitness.rb
@@ -20,4 +20,49 @@ module Fitness
       end**(0.5)
     end
   end
+
+# {"front_end" => [total,language ], back_end: [total, language], database: [total, language], cost: pph}
+
+  def self.developers_fitness(contractor_opts)
+    Proc.new do |dna|
+      correct_language_bonus = total_with_named_language(contractor_opts, dna)
+      skill_bonus = total_skill_rate(dna)/5.0
+      cost_bonus = total_cost_difference(contractor_opts, dna)
+      team_breakdown_bonus = team_breakdown_difference(contractor_opts, dna)
+    end
+  end
+
+  def total_with_named_language(contractor_opts, dna)
+    dna.select do |dev|
+      languages = dev.tags.map{|x| x.name}
+      languages.include?(contractor_opts[dev.specialty.url_name][1])
+    end.count
+  end
+
+  def total_skill_rate(dna)
+    dna.reduce(0) do |acc, dev|
+      acc + dev.skills_rating
+    end
+  end
+
+  def total_cost_difference(contractor_opts, dna)
+    actual_cost = dna.map{|dev| dev.rate}.reduce(&:+)
+    expected_cost = contractor_opts[:cost]
+    expected_cost - actual_cost
+  end
+
+  def team_breakdown_difference(contractor_opts, dna)
+    actual_team = dna.reduce(Hash.new(0)) do |acc, dev|
+      acc[dev.specialty.url_name] += 1
+    end
+
+    expected_team = contractor_opts.map do |key, val|
+      [key, val[0]] if key != :cost
+    end.to_h
+
+    differences = actual_team.merge(expected_team){|k, v1, v2| (v1-v2)**2}
+    differences.reduce(0) do |acc, k, v|
+      acc + v
+    end
+  end
 end
--- test/genetic_algorithm_tests/fitness_test.rb
+++ test/genetic_algorithm_tests/fitness_test.rb
@@ -12,4 +12,46 @@ class FitnessTest < Minitest::Test
     assert_equal 2**(0.5), Fitness.euc_dist([0,0])[[1,1]]
     assert_equal 5, Fitness.euc_dist([3,3])[[0,-1]]
   end
+
+
+  def total_with_named_language_test
+    assert_equal 4, total_with_named_language(contractor_opts, hypothesis_dna)
+  end
+
+private
+
+  def contractor_opts
+    {
+      "front_end" => [3,"Javascript"],
+      "back_end" => [1,"Ruby"],
+      "database" => [3,"Postgresql"],
+      cost: 150
+    }
+  end
+
+  def langs
+    [
+      Tag.create(name: "Javascript"),
+      Tag.create(name: "Ruby"),
+      Tag.create(name: "Postgresql"),
+      Tag.create(name: "Elixir"),
+      Tag.create(name: "Sass")
+    ]
+  end
+
+  def specialties
+    @list ||= create_list(:specialty, 3)
+  end
+
+  def hypothesis_dna
+    [
+      Developer.create(name: "1", last_name: "1", tags: langs[0..1], email: "1", rate: 10, specialty: specialties[0]),
+      Developer.create(name: "2", last_name: "2", tags: langs[0]+langs[4], email: "2", rate: 20, specialty: specialties[1]),
+      Developer.create(name: "3", last_name: "3", tags: langs[2..3], email: "3", rate: 10, specialty: specialties[2]),
+      Developer.create(name: "4", last_name: "4", tags: langs[3..4], email: "4", rate: 30, specialty: specialties[0]),
+      Developer.create(name: "5", last_name: "5", tags: langs[3], email: "5", rate: 20, specialty: specialties[0]),
+      Developer.create(name: "6", last_name: "6", tags: langs[1..2], email: "6", rate: 10, specialty: specialties[1]),
+      Developer.create(name: "7", last_name: "7", tags: langs[1..4], email: "7", rate: 40, specialty: specialties[2]),
+    ]
+  end
 end
