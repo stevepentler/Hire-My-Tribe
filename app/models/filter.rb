de7520fb7bdc9ea6f951c08fea527d7c89a195c6
Aaron Greenspan <afg419@gmail.com>
2016-01-20 11:03:33 -0700

    Refactors filtering methods to more easily play with params

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,7 +1,6 @@
 module Filter
   def self.sort_filter(params)
-    binding.pry
-    developers = sort_developers(params)
+    developers = sort_developers(params) if sort_params(params)
     tags = selected_tags(params)
 
     tags.inject(developers) do |acc, tag|
@@ -9,25 +8,22 @@ module Filter
     end
   end
 
-  def self.sort_developers(params)
-    Developer.order(sort_params_to_sort_options(params))
+  def self.sort_params(params)
+    params["filter"] ||= {}
+    params["filter"]["sort"]
   end
 
-  def self.sort_params_to_sort_options(params)
-    [params["filter"]["sort"].split.map(&:to_sym)].to_h
+  def self.sort_developers(params)
+    Developer.order(active_record_sort_options(params))
   end
-  #
-  # def self.sort
-  #   {
-  #     "Rate: Ascending" => lambda { |active| active.order(rate: :asc) },
-  #     "Rate: Descending" => lambda { |active| active.order(rate: :desc) }
-  #   }
-  # end
-  #
+
+  def self.active_record_sort_options(params)
+    [sort_params(params).split.map(&:to_sym)].to_h
+  end
+
   def self.selected_tags(params)
-    params["filter"] ||= {}
-    params["filter"].select{ |key, val| val == "1" }.keys.map do |tag_name|
-      Tag.includes(:developers).find_by(name: tag_name)
+    params["filter_tag_id"].keys.map do |tag_id|
+      Tag.includes(:developers).find(tag_id)
     end
   end
 end
