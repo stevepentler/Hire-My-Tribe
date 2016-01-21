31208ba94d5387267f15a9ebdc726bad231700ee
Steve Pentler <stevepentler@gmail.com>
2016-01-21 08:48:24 -0700

    Adds url to config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,9 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESSS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :url => ':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
   }
 
   # Send deprecation notices to registered listeners.
