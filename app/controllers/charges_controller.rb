5d8f9905a79e79ba2378699fe161a86572484750
Steve Pentler <stevepentler@gmail.com>
2016-01-22 11:20:52 -0700

    Removes extraneous files, disallows adding inactive developer to project

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,7 @@
 class ChargesController < ApplicationController
   def new
     @project = Project.find(params[:project_id])
-    if @project.developers.any? {|dev| dev.status == 'unavailable'}
+    if @project.developers.any? {|dev| dev.status != 'available'}
       flash[:error] = "Some of your developers are unavailable"
       redirect_to contractor_project_path(@project)
     else
@@ -23,8 +23,16 @@ class ChargesController < ApplicationController
       :currency    => 'usd'
     )
     redirect_to contractor_project_payment_path(@project)
-    flash[:payment] = "Thank you for your payment of #{project_cost(@project)}"
+    flash[:payment] = "Thank you for your payment of $#{project_cost(@project)}"
   rescue Stripe::CardError => e
     flash[:error] = e.message
+  end
+
+  private 
+
+  def has_unavailable_developer?
+    developer.any? {|dev| dev.status != 'available'}
+    flash[:error] = "Some of your developers are unavailable"
+    redirect_to contractor_project_path(@project)
   end
 end
